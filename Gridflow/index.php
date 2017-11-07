6849b9a1109bfa5f5d4e9e03e486aa7d4cd89b5d
Tanel Nigul <tanel.nigul@gmail.com>
2017-11-07 17:54:25 +0200

    Add files via upload

---
+++ Gridflow/index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+
+<html>
+  <meta http-equiv="Cache-control" content="no-cache">
+  <meta charset="UTF-8">
+  <meta name="description" content="Gridflow">
+  <meta name="keywords" content="The Better New Way to Manage Projects and Sales">
+  <meta name="author" content="PauseDigital">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/custom.css">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  <link rel="icon" href="img/favicon.png" type="image/png"/>
+  <link rel="shortcut icon" href="img/favicon.png" type="image/png"/>
+  <script src="https://use.fontawesome.com/cd3d33bd0e.js"></script>
+  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+  <script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+  <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+  <script src="js/custom.js"></script>
+  <body>
+
+<head>
+  <title>Gridflow - The Better New Way to Manage Projects</title>
+</head>
+
+<div id="logo"></div>
+
+<ul class="nav justify-content-end">
+<li class="nav-item">
+  <a class="nav-link active" href="#">Features</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">Pricing</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">Support</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">Forums</a>
+</li>
+</ul>
+
+<div id="header">
+
+  <h4 class="header">Coming Soon</h4>
+
+  <h1 class="header">New Era CRM</h1>
+
+  <div class="divider white"></div>
+
+  <h5 class="header">The better new way to manage project <br>and sales funnels. Say hello to efficiency.</h5>
+
+</div>
+
+<div id="cta">
+
+  <h5 class="cta">Gridflow is not quite ready yet. Sign up to get <strong>updates on
+the project</strong> and be <strong>notified when it’s ready</strong>.</h5>
+
+<form>
+  <div class="form-inline">
+
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter e-mail">
+    <button type="submit" class="btn btn-primary cta">NOTIFY ME!</button>
+    <small id="emailHelp" class="form-text text-muted">*We hate spam too, so we'll only send you the information you need.</small>
+
+
+  </div>
+</form>
+
+</div>
+
+<div class="container">
+
+  <div class="row">
+    <div class="col">
+  <h2 class="main">Are you ready for Gridflow?</h2>
+
+  <div class="divider"></div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col">
+
+  <p>Gridflow is created for professional salespeople and project managers, who wish to
+  make their work more efficient. The web application lets you create and manage
+  big or small projects with ease, keeping track of leads and sales funnels.</p>
+
+  <p>Specimen book. It has survived not only five centuries, but also the leap into
+    electronic typesetting, remaining essentially unchanged. It was popularised in the
+    1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
+    recently with desktop publishing software like Aldus PageMaker including
+    versions of Lorem Ipsum.</p>
+
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col">
+
+  <h2 class="main">Want to invest?</h2>
+
+  <div class="divider"></div>
+
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col">
+
+  <p>Gridflow is created for professional salespeople and project managers, who wish to
+  make their work more efficient. The web application lets you create and manage
+  big or small projects with ease, keeping track of leads and sales funnels.</p>
+
+    </div>
+  </div>
+
+</div>
+
+<div class="container footer">
+
+  <div class="row">
+    <div class="col">
+      <h5 class="footer">NAVIGATION</h5>
+      <ul class="footer">
+        <li class="footer"><a class="footer" href="#">Features</a></li>
+        <li class="footer"><a class="footer" href="#">Pricing</a></li>
+        <li class="footer"><a class="footer" href="#">Support</a></li>
+        <li class="footer"><a class="footer" href="#">Forums</a></li>
+      </ul>
+    </div>
+    <div class="col">
+      <h5 class="footer">PRESS</h5>
+      <ul class="footer">
+        <li class="footer"><a class="footer" href="#">Press content</a></li>
+        <li class="footer"><a class="footer" href="#">Terms & conditions</a></li>
+        <li class="footer"><a class="footer" href="#">Contact</a></li>
+      </ul>
+    </div>
+    <div class="col">
+      <h5 class="footer">DEVELOPMENT</h5>
+      <ul class="footer">
+        <li class="footer"><a class="footer" href="#">Admin panel</a></li>
+        <li class="footer"><a class="footer" href="#">Testing zone</a></li>
+        <li class="footer"><a class="footer" href="#">Contact developers</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col">
+      <p class="copyright">2017 © PauseDigital Ltd. All rights reserved.</p>
+    </div>
+  </div>
+
+</div>
+
+</body>
+</html>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Muli|Raleway');
+</style>
